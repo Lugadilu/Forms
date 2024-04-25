be6062c98645781b22c94ba93db91bd61999c87e
Lugadilu <emmanuellugadilu.com>
2024-04-24 23:52:31 -0700

    Added the FormRecord to database and the endpoints

---
+++ FormAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FormAPI.Models;
+
+namespace FormAPI.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //can addd connection string but i choose to add it in appsettings.json
+        }
+
+        public DbSet<FormField> FormFields { get; set; }
+        //public DbSet<FormRecord> FormRecords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ignore the Rules property of the FormField entity
+            modelBuilder.Entity<FormField>().Ignore(f => f.Rules);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FormAPI/Program.cs
@@ -0,0 +1,49 @@
+using FormAPI.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Register ApplicationContext
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); // Removed the misplaced parameters
+
+// Creating form fields
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Forms}/{action=Index}/{id?}");
+
+
+app.Run();
+
+
+
+
+
